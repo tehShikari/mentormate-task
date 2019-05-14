95a8017e627d26b00d5be9ca1ebd48120199ec82
tehShikari <teh.shikari@gmail.com>
2019-05-14 19:31:09 +0300

    Added prompt and commented out the for() + additional comment

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-var n = [];
-for (n=2; n<10000; n++) {
-    if(n%2!=0){
-        console.log("---***---***------***---***---");
-        console.log("--*****-*****----*****-*****--");
-        console.log("-***-*****-***--***-*****-***-");
-        console.log("***---***---******---***---***");
-    }
-}
+var n = prompt("Please type in an odd number:");
+//for (n=2; n<10000; n++) {
+//    if(n%2!=0){
+//        console.log("---***---***------***---***---");
+//        console.log("--*****-*****----*****-*****--");
+//        console.log("-***-*****-***--***-*****-***-");
+//        console.log("***---***---******---***---***");
+//    }
+//}
 
 //the code should generate as many stars as the number is + 2 on the bottom row + 2 on the bottom row
 
+//the code should be with addEventListener() maybe?
