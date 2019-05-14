282bfe1228b65c09c94a9163664e02451cb286f3
tehShikari <teh.shikari@gmail.com>
2019-05-14 15:53:06 +0300

    Set up index.js with initial idea

---
+++ index.js
@@ -0,0 +1,12 @@
+var n = [];
+for (n=2; n<10000; n++) {
+    if(n%2!=0){
+        console.log("---***---***------***---***---");
+        console.log("--*****-*****----*****-*****--");
+        console.log("-***-*****-***--***-*****-***-");
+        console.log("***---***---******---***---***");
+    }
+}
+
+//the code should generate as many stars as the number is + 2 on the bottom row + 2 on the bottom row
+
