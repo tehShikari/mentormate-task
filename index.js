3e91aca635f0d68986082d1e96a1958d34aa4228
tehShikari <teh.shikari@gmail.com>
2019-05-17 18:08:22 +0300

    Added ideas of the function to use, with the best idea not commented

--- index.js
+++ index.js
@@ -1,4 +1,52 @@
-var n = prompt("Please type in an odd number:");
+//***the code should generate as many stars as the number is + 2 on the bottom row + 2 on the bottom row
+
+//***the code should be with addEventListener() maybe?
+
+//var oddNumberCheck = "";
+
+//var numOdd = prompt("Please type in an odd number:");
+//var oddNum = Number(stringOdd);
+
+//// check if guess is right
+//if(oddNum === oddNumberCheck) {
+	
+//}
+//*** otherwise you got it wrong, check if lower:
+//else if (guess > oddNumberCheck) {
+//	alert("Too high, try again!");
+//}
+
+//*** otherwise you got it wrong, check if higher:
+//else {
+//	alert("Too low, try again!");
+//}
+
+//function isOdd(num) { return num % 2;}
+//console.log("1 is " + isOdd(1));
+//console.log("2 is " + isOdd(2));
+//console.log("3 is " + isOdd(3));
+//console.log("4 is " + isOdd(4));
+
+
+//function isEven(value){
+//    if (value%2 == 0)
+//        return false;
+//    else
+//        return true;
+//}
+
+
+//***most probable n()
+function isntEven(n){
+while(n <=333){
+	if(n % 2 !== 0){
+	console.log(n);
+	}
+	n++;
+}
+}
+
+
 //for (n=2; n<10000; n++) {
 //    if(n%2!=0){
 //        console.log("---***---***------***---***---");
@@ -8,6 +56,15 @@ var n = prompt("Please type in an odd number:");
 //    }
 //}
 
-//the code should generate as many stars as the number is + 2 on the bottom row + 2 on the bottom row
 
-//the code should be with addEventListener() maybe?
+
+
+
+
+
+
+
+
+
+
+
