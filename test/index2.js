f70d8a433fe4a1cd7b1c88ab459eb0a056f6434c
tehShikari <teh.shikari@gmail.com>
2019-05-20 10:13:25 +0300

    func firstRow(x) in index2.js works and mofified index.js

--- test/index2.js
+++ test/index2.js
@@ -1,3 +1,15 @@
-var logo = ["-", "*"];
+var logo = ["-", "*"],
+    x = 0;
+
 
 console.log(logo[0] + logo[1]);
+
+function firstRow(x) {
+    var row = console.log(logo[0].repeat(x) + logo[1].repeat(x) + logo[0].repeat(x) + logo[1].repeat(x) + logo[0].repeat(x));
+}
+
+function oddNum(x) {
+    if (x % 2 !== 0) {
+        firstRow(x);
+    }
+}
