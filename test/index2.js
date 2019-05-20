ba3af46054a3040c2cb007d7d0cd52e0480494c6
tehShikari <teh.shikari@gmail.com>
2019-05-20 10:30:28 +0300

    Updated firstRow()

--- test/index2.js
+++ test/index2.js
@@ -1,11 +1,18 @@
 var logo = ["-", "*"],
     x = 0;
 
-
 console.log(logo[0] + logo[1]);
 
 function firstRow(x) {
-    var row = console.log(logo[0].repeat(x) + logo[1].repeat(x) + logo[0].repeat(x) + logo[1].repeat(x) + logo[0].repeat(x));
+    var x = x;
+    var row = logo[0].repeat(x) + logo[1].repeat(x) + logo[0].repeat(x) + logo[1].repeat(x) + logo[0].repeat(x);
+    if (x % 2 !== 0) {
+        var indexDash = logo.indexOf("-");
+        var indexStar = logo.indexOf("*");
+        console.log(indexDash);
+        console.log(indexStar);
+        console.log(x);
+    }
 }
 
 function oddNum(x) {
