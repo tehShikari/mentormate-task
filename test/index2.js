07f40efb9b991a1054e373e90ba763dc07292e66
tehShikari <teh.shikari@gmail.com>
2019-05-20 13:05:09 +0300

    Updated index2.js, but still not working; edited index.js

--- test/index2.js
+++ test/index2.js
@@ -1,17 +1,22 @@
 var logo = ["-", "*"],
-    x = 0;
+    x = 0,
+    minVal = 2,
+    maxVal = 10000;
 
 console.log(logo[0] + logo[1]);
 
 function firstRow(x) {
     var x = x;
     var row = logo[0].repeat(x) + logo[1].repeat(x) + logo[0].repeat(x) + logo[1].repeat(x) + logo[0].repeat(x);
-    if (x % 2 !== 0) {
+    if (x % 2 !== 0 && x > minVal && x < maxVal) {
         var indexDash = logo.indexOf("-");
         var indexStar = logo.indexOf("*");
-        console.log(indexDash);
-        console.log(indexStar);
-        console.log(x);
+        function rowBuilder (logo) {
+            var nextRow = indexDash.repeat(x--);
+            //doesnt work .. ?
+            console.log(typeof(nextRow));
+            return nextRow;
+        }
     }
 }
 
